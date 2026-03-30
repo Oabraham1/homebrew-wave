3ea4620691f682a40c3aaf9ed22fb569d1e4e3b1
Oabraham1 <oabraham@kalshi.com>
2026-03-30 00:28:20 -0400

    wave v0.1.1

--- Formula/wave.rb
+++ Formula/wave.rb
@@ -6,16 +6,16 @@ class Wave < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Oabraham1/wave/releases/download/v0.1.1/wave-macos-arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
       url "https://github.com/Oabraham1/wave/releases/download/v0.1.1/wave-macos-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
   on_linux do
     url "https://github.com/Oabraham1/wave/releases/download/v0.1.1/wave-linux-x86_64.tar.gz"
-    sha256 "PLACEHOLDER_SHA256"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   end
 
   def install
